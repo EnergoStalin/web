87641008f24ffc0ae3400b5f2dcf6a87dc5d3c92
EnergoStalin <alex.vipers@mail.ru>
2019-12-25 01:19:35 +0300

    Добавлен белый фон документу, убрана подгрузка изображений из сторонних источников

--- Quest/options.js
+++ Quest/options.js
@@ -6,22 +6,10 @@ var SimulateOptions = {
 }
 var EnvironmentOptions = {
 	assets_url: [
-		{
-			name: "background1",
-			url: "C:\\Users\\Alex\\Documents\\Wallpaper\\osu-hd-wallpapers.jpg"
-		},
-		{
-			name: "background3",
-			url: "C:\\Users\\Alex\\Documents\\Programming\\Js\\sc_005.png"
-		},
-		{
-			name: "background4",
-			url: "https://mdn.mozillademos.org/files/225/Canvas_drawimage.jpg"
-		},
-		{
-			name: "background5",
-			url: "https://sun9-2.userapi.com/c846123/v846123194/16ed12/9Kx9zBO6R0I.jpg"
-		}
+		/*{
+			name: ,
+			url: 
+}*/
 	],
 	findAsset: function(name) {
 		for(var i = 0; i < this.assets.length; i++) {
