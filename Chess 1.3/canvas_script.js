d2b62120083b08c49c51adfc89400bb85feaaf98
EnergoStalin <alex.vipers@mail.ru>
2020-04-04 21:39:53 +0300

    Sat_4_Apr_21:39:53_MSK_2020

--- Chess 1.3/canvas_script.js
+++ Chess 1.3/canvas_script.js
@@ -1,179 +1,112 @@
-var tileset = new Image();
-var turn = true;
-var tile = [];
-var choice = -1;
-var Viewport = {x: 140,y: 40};
-var Coord = function(x,y) {
-	this.x = x;
-	this.y = y;
-	this.selected = false;
-	this.contain = function(x,y,size) {
-		if(x > this.x && x < (this.x + size) && y > this.y && y < (this.y + size)) return true;
-		return false;
-	}
+var load_images = function(arr, callback) {
+	let total = arr.length,
+	res = [];
 	
-	return this;
+	let onload = function() {
+		if(--total == 0) { console.log("Loaded... ",total); callback(res); }
+	}
+
+	for(let i = 0; i < arr.length; i++)
+	{
+		res.push(new Image());
+		res[i].addEventListener("load", onload);
+		res[i].src = arr[i];
+	}
 }
+var _Chess = function() {
+	//context
+	var that = this;
+	//Classes
+	this.Coord2D = function(x,y) {
+		this.x = x;
+		this.y = y;
+		this.contain = function(c,size) {
+			if(c.x > this.x && c.x < (this.x + size) && y > this.y && y < (this.y + size)) return true;
+			return false;
+		}
+		
+		return this;
+	}
+	this.viewport = function() {
+		var that2 = this;
+		this.x = this.y = 0;
+		this.size =  {
+			w: 800,
+			h: 600
+		};
+		this.canvas = document.createElement("canvas");
+		this.canvas.style = "width: 800px; height: 600px;";
+		document.body.appendChild(this.canvas);
+		this.ctx = this.canvas.getContext("2d");
+		this.background = "gray";
+		this.draw = function() {
+			that2.ctx.fillStyle = that2.background;
+			that2.ctx.fillRect(0,0,that2.size.w,that2.size.h);
+			that2.ctx.drawImage(that.tileset.img,0,0,that.tileset.img.width/2,that.tileset.img.height/2);
+		}
+	};
 
-var FigureCoord = {
-	"quinn": new Coord(0,0),
-	"king": new Coord(64,0),
-	"rook": new Coord(64*2,0),
-	"horse": new Coord(64*3,0),
-	"elephant": new Coord(64*4,0),
-	"pawn": new Coord(64*5,0)
-}
+	this.viewport = new this.viewport();
 
-var FigureSet = [
-	"rook",
-	"horse",
-	"elephant",
-	"quinn",
-	"king",
-	"elephant",
-	"horse",
-	"rook",
-]
+	//sprites
+	this.sprites = [
+		"SPRITES/memI0.png"
+	];
 
-var Check = function(type,coord) {
-	this.size = 64;
-	this.type = type;
-	this.coord = coord;
-	this.absCoord = null;
+	//chess order
+	this.order = [
+		"rook",
+		"horse",
+		"elephant",
+		"quinn",
+		"king",
+		"elephant",
+		"horse",
+		"rook",
+	];
+
+	//UV coord
+	this.tilesetUV = {
+		"quinn": new this.Coord2D(0,0),
+		"king": new this.Coord2D(64,0),
+		"rook": new this.Coord2D(64*2,0),
+		"horse": new this.Coord2D(64*3,0),
+		"elephant": new this.Coord2D(64*4,0),
+		"pawn": new this.Coord2D(64*5,0)
+	};
+
+	//Chess desk
+	this.desk = [];
+
+	//tileset
+	this.tileset = {size: 64, url: "", img: null, load: function(l)
+		{
+			that.tileset.url = l[0].src;
+			that.tileset.img = l[0];
+
+			that.init(null);
+		}
+	};
+
+	this.loop = function() {
+		that.viewport.draw();
+
+		requestAnimationFrame(that.loop);
+	}
+
+	//Init function
+	this.init = function(evt)
+	{
+		if(evt != null)
+		{
+			load_images(that.sprites,that.tileset.load);
+			return;
+		}
+		
+		requestAnimationFrame(that.loop);
+	}
 };
 
-var canvas = document.createElement("canvas");
-canvas.width = 800;
-canvas.height = 600;
-var ctx = canvas.getContext("2d");
+var Chess = new _Chess();
 
-function getRelativeMousePosition(mousex,mousey) {
-	var rect = canvas.getBoundingClientRect();
-	return {x: mousex - rect.left - Viewport.x, y: mousey - rect.top - Viewport.y}
-}
-
-function Draw() {
-	ctx.clearRect(0,0,canvas.width,canvas.height);
-	ctx.fillStyle = "yellow";
-	for(var i = 0,cur = 0; i < 8; i++) {
-		for(var j = 0; j < 8; j++,cur++) {
-			if(tile[cur].selected) ctx.fillRect(tile[cur].absCoord.x,tile[cur].absCoord.y,tile[cur].size,tile[cur].size);
-			if(tile[cur].type != -1) {
-				ctx.drawImage(
-					tileset,
-					tile[cur].coord.x,
-					tile[cur].coord.y,
-					tile[cur].size,
-					tile[cur].size,
-					tile[cur].absCoord.x,//-Viewport.x,
-					tile[cur].absCoord.y,//-Viewport.y,
-					tile[cur].size,
-					tile[cur].size
-				);
-			}
-		}
-	}
-}
-
-function Turn() {
-
-}
-
-//***************************run***************************
-
-var Win = function() {
-	return true;
-}
-
-var run = function() {
-	Draw();
-	while(!Win()) {
-		if(turn) {
-			
-		} else {
-
-		}
-	}
-}
-
-//**************************init***************************
-
-var init = function() {
-	tileset.src = "SPRITES/memI0.png"
-	tileset.addEventListener("load",run);
-	for(var i = 0; i < 8; i++) {
-		tile.push(new Check(0,FigureCoord[FigureSet[i]]));
-	}
-	for(var i = 0; i < 8; i++) {
-		tile.push(new Check(0,FigureCoord["pawn"]));
-	}
-	for(var i = 0; i < 32; i++) {
-		tile.push(new Check(-1,new Coord(0,0)));
-	}
-	for(var i = 8; i > 0; i--) {
-		tile.push(new Check(1,new Coord(FigureCoord["pawn"].x,FigureCoord["pawn"].y+64)));
-	}
-	for(var i = 8; i > 0; i--) {
-		tile.push(new Check(1,new Coord(FigureCoord[FigureSet[i-1]].x,FigureCoord[FigureSet[i-1]].y+64)));
-	}
-	for(var i = 0,cur = 0; i < 8; i++) {
-		for(var j = 0; j < 8; j++,cur++) {
-			tile[cur].absCoord = new Coord(tile[cur].size*j+Viewport.x, tile[cur].size*i+Viewport.y);
-		}
-	}
-
-	document.body.appendChild(canvas);
-}
-//Swap
-
-var swap = function(one,two) {
-	var  temp = two;
-	two = one;
-	one = temp;
-}
-
-//animate
-
-var Animate = function(i, choice) {
-	var timer = setInterval(function() {
-		console.log(tile[choice].absCoord.x,tile[choice].absCoord.y,tile[i].absCoord.x,tile[i].absCoord.y);
-		if(tile[choice].absCoord.x > tile[i].absCoord.x){
-			tile[choice].absCoord.x--;
-		} else if(tile[choice].absCoord.x < tile[i].absCoord.x) tile[choice].absCoord.x++;
-		if(tile[choice].absCoord.y > tile[i].absCoord.y){
-			tile[choice].absCoord.y--;
-		} else if(tile[choice].absCoord.y < tile[i].absCoord.y) tile[choice].absCoord.y++;
-		Draw();
-		if(tile[choice].absCoord.y == tile[i].absCoord.y && tile[choice].absCoord.x == tile[i].absCoord.x) clearInterval(timer);
-	},100);
-}
-
-//************************listeners************************
-
-canvas.addEventListener("click",function(evt) {
-	//var win = null;
-	//win = Win();
-	//if(win == null) {
-		var mouse = getRelativeMousePosition(evt.x,evt.y);
-		mouse.x += Viewport.x;
-		mouse.y += Viewport.y;
-		for(var i = 0; i < 64; i++) {
-			if(tile[i].absCoord.contain(mouse.x,mouse.y,tile[i].size)) {
-				if(choice != -1) {
-					Animate(i,choice);
-					tile[choice].selected = false;
-					tile[i].selected = false;
-					tile[i].absCoord.x = tile[choice].absCoord.x;
-					tile[i].absCoord.y = tile[choice].absCoord.y;
-					choice = -1;
-				} else {
-					tile[i].selected = !tile[i].selected;
-					choice = i;
-				}
-				Draw();
-				break;
-			}
-		}
-	//}
-});
-window.addEventListener("DOMContentLoaded",init);
+window.addEventListener("DOMContentLoaded",Chess.init);
